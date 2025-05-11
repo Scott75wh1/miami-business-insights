e5234cb7c5a621c38ce52d8b719bd620c400f98e
Scott75wh1 <156372602+Scott75wh1@users.noreply.github.com>
2025-05-11 19:38:32 -0400

    Add data/trends.py with fetch_google_trends()

---
+++ data/trends.py
@@ -0,0 +1,23 @@
+from pytrends.request import TrendReq
+import pandas as pd
+
+# Inizializza PyTrends
+pytrends = TrendReq(hl='en-US', tz=360)
+
+def fetch_google_trends(keyword: str,
+                        timeframe: str = 'today 12-m',
+                        geo: str = 'US-FL-12086') -> pd.DataFrame:
+    """
+    Recupera i dati di Google Trends per una keyword:
+    - keyword: termine di ricerca
+    - timeframe: intervallo (es. 'now 7-d', 'today 12-m')
+    - geo: area geografica (DMA Miami-Fort Lauderdale = 'US-FL-12086')
+    Ritorna un DataFrame con indice temporal e colonna 'trend_volume'.
+    """
+    pytrends.build_payload([keyword], timeframe=timeframe, geo=geo)
+    data = pytrends.interest_over_time()
+    if data.empty:
+        return pd.DataFrame()
+    # Rimuove la colonna isPartial
+    return data.drop(columns=['isPartial'], errors='ignore') \
+               .rename(columns={keyword: 'trend_volume'})
